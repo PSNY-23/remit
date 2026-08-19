91e9c3304c15fcf7f67eb33807b91c3b256cabe6
pankaj <pankajteceract@gmail.com>
2026-08-19 18:00:47 +0530

    Add landing page

---
+++ app/_meta.ts
@@ -0,0 +1,13 @@
+export default {
+  index: {
+    type: 'page',
+    display: 'hidden',
+    theme: {
+      sidebar: false,
+      toc: false,
+      breadcrumb: false,
+      pagination: false,
+      layout: 'full',
+    },
+  },
+};
